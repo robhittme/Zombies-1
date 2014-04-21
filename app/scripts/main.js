b89091fa4007c999b94924e6761ca64d774eeab9
Ryan McGovern <ryanmcgovern@Ryans-MacBook-Air.local>
2014-04-21 16:55:11 -0400

    Merge branch 'master' of https://github.com/TIY-Q2-FEE-CHS/Zombies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(".btn").click(function(e) {
+	e.preventDefault();
+	console.log("im working");
+	var hours = $(".sleepSelect").val();
+	var varSleep = new Sleep(hours);
+	var cups = $(".coffeeSelect").val();
+	var varCoffee= new Coffee(cups);
+	var difficulty = $(".classroomSelect").val();
+	var varClassroom = new Classroom(difficulty);
+
+ 
+
+varLife= (varSleep.life + varCoffee.life + varClassroom.life);
+$(".results").append("<li>" + varLife + "</li>");
+});

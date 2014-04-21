8eb0bf9b4848e559e702f7d7875068c24f5e7d61
Chelsie Erickson <cme3888@Chelsies-MacBook-Air.local>
2014-04-21 15:09:19 -0400

    merged upstream

--- app/scripts/constructors.js
+++ app/scripts/constructors.js
@@ -1,3 +1,21 @@
+function Sleep(hours){
+	this.hours = (hours) * 10;
+}
+function Classroom(difficulty){
+	this.difficulty = function(){
+		if(difficulty === 'easy'){
+			this.life = this.life - 20;
+		}
+		if(difficulty === 'intermediate'){
+			this.life = this.life - 40;
+		}
+		if(difficulty === 'hard'){
+			this.life = this.life - 60;
+		}
+	}
+}
+
+
 function Coffee(cups) {
 	this.cups = cups;
 	this.life = this.life + (cups*5);

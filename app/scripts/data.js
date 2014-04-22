9fe0edfa7642d0e58c34262337463f354cecb40b
Robert Hitt <rphitt1@gmail.com>
2014-04-22 13:45:52 -0400

    added my info to data.js

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -8,11 +8,11 @@ var aboutData= [
 
 	},
 	{
-		photo:
-		name:Robert;
-		title:
-		hometown:
-		nickname:
+		photo: url("http://i1.sndcdn.com/avatars-000043348921-lnmuj4-crop.jpg?77d7a69");
+		name: Robert P. Hitt;
+		title: programmer;
+		hometown: Greenville;
+		nickname: R2;
 	},
 	{
 		photo:

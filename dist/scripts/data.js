1d7957aaa3e5e1e54e15d25187d8ab6d3ae9f429
Robert Hitt <rphitt1@gmail.com>
2014-07-01 11:59:17 -0400

    changes to the dist

---
+++ dist/scripts/data.js
@@ -0,0 +1,26 @@
+var aboutData= [ 
+	{
+		"photo":"https://scontent-b.xx.fbcdn.net/hphotos-frc3/t1.0-9/376663_10150451842794789_676954083_n.jpg",
+		"name": "Name: Chelsie Erickson",
+		"title": "Programmer",		
+		"hometown": "Hometown: San Diego",
+		"twitter": "http://assets.bizjournals.com/bizjournals/Twitter315*304.jpg?v=1",
+		"twitterhandle":"http://twitter.com/chel_sees3888"
+	},
+	{
+		"photo": "http://i1.sndcdn.com/avatars-000043348921-lnmuj4-crop.jpg?77d7a69",
+		"name": "Name: Robert P. Hitt",
+		"title": "Programmer",
+		"hometown": "Hometown: Greenville",
+		"twitter": "http://assets.bizjournals.com/bizjournals/Twitter315*304.jpg?v=1",
+		"twitterhandle":"http://twitter.com/RobHittMe"
+	},	
+	{
+		"photo":"https://scontent-b.xx.fbcdn.net/hphotos-prn1/t1.0-9/1013733_1404932253092644_1166472884_n.jpg",
+		"name": "Name: Ryan McGovern",
+		"title": "Programmer",
+		"hometown": "Hometown: Rockville Centre",
+		"twitter": "http://assets.bizjournals.com/bizjournals/Twitter315*304.jpg?v=1",
+		"twitterhandle":"http://twitter.com/RyanNotBryan"
+	}
+];
---
+++ dist/scripts/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+$(".btn").click(function(e) {
+	e.preventDefault();
+	console.log("im working");
+	var hours = Number($(".sleepSelect").val());
+	var varSleep = new Sleep(hours);
+	
+	var cups = Number($(".coffeeSelect").val());
+	var varCoffee= new Coffee(cups);
+
+	var cans = Number($(".energySelect").val());
+	var varEnergyDrink = new Energydrink(cans);
+
+	var amount = Number($(".snackSelect").val());
+	var varSnacks = new Snacks(amount);
+
+	var difficulty = $(".classroomSelect").val();
+	var varClassroom = new Classroom(); 
+
+	varClassroom.difficulty(difficulty, varSleep);
+	console.log(varSleep.life);
+
+	var levelOfToughness = $(".assignmentSelect").val();
+	var varAssignment = new AssignmentDifficulty()
+	varAssignment.levelOfToughness(levelOfToughness, varSleep);
+	console.log(varSleep.life)
+
+var varLife=(varCoffee.life + varSleep.life + varEnergyDrink.life + varSnacks.life);
+console.log(varCoffee.life);
+console.log(varClassroom.life);
+console.log(varEnergyDrink.life);
+console.log(varSnacks.life);
+$(".results").children().remove();
+$(".results").append("<li>" + varLife + "</li>");
+	if (varLife <= 30) {
+		$(".alert").text("Your alertness is low, you should get some sleep, get food, or a drink!");
+	}
+
+});
+
+var aboutTemplate = _.template ($('#aboutInfo').html(),aboutData);
+				$('.col-lg-5').append(aboutTemplate);
+
+});
+
+
+
+
+
+
+
